d07ca90d8082b00a2d85433dfba8229063999da5
Reechat <52690502+Reechat@users.noreply.github.com>
2020-06-01 22:47:53 +0600

    Creating a stopwatch algorithm and connecting start and stop buttons

--- Счетчик/Form1.cs
+++ Счетчик/Form1.cs
@@ -26,18 +26,33 @@ namespace Счетчик
         }
         private void Start1_Click(object sender, EventArgs e)
         {
-           
+            timer2.Stop();
+            timer3.Stop();
+            if (timer1.Enabled)
+                timer1.Enabled = false;
+            else
+                timer1.Enabled = true;
 
         }
 
         private void start3_Click(object sender, EventArgs e)
         {
-            
+            timer2.Stop();
+            timer1.Stop();
+            if (timer3.Enabled)
+                timer3.Enabled = false;
+            else
+                timer3.Enabled = true;
         }
 
         private void start2_Click(object sender, EventArgs e)
         {
-            
+            timer1.Stop();
+            timer3.Stop();
+            if (timer2.Enabled)
+                timer2.Enabled = false;
+            else
+                timer2.Enabled = true;
         }
 
 
@@ -86,7 +101,23 @@ namespace Счетчик
 
         public void timer1_Tick(object sender, EventArgs e)
         {
-            
+            ++s2;
+            if (s2 == 60.0)
+            {
+                s2 = 0.0;
+                ++m2;
+            }
+            double num1 = Convert.ToDouble(text2.Text);
+            if (s2 > 0.0)
+            {
+                x2 += num1 / 3600.0;
+                Value2.Text = x2.ToString();
+            }
+            if (m2 == 60.0)
+            {
+                m2 = 0.0;
+                ++h2;
+            }
 
 
 
@@ -95,13 +126,47 @@ namespace Счетчик
         private void timer2_Tick(object sender, EventArgs e)
         {
             {
-                
+                ++s4;
+                if (s4 == 60.0)
+                {
+                    s4 = 0.0;
+                    ++m4;
+                }
+                double num1 = Convert.ToDouble(text4.Text);
+                if (s4 > 0.0)
+                {
+                    x4 += num1 / 3600.0;
+                    Value3.Text = Convert.ToString(x4);
+                }
+                if (m4 == 60.0)
+                {
+                    m4 = 0.0;
+                    ++h4;
+                }
+
             }
         }
 
         private void timer3_Tick(object sender, EventArgs e)
         {
-            
+            ++s3;
+            if (s3 == 60.0)
+            {
+                s3 = 0.0;
+                ++m3;
+            }
+            double num1 = Convert.ToDouble(text3.Text);
+            if (s3 > 0.0)
+            {
+                x3 += num1 / 3600.0;
+                Value4.Text = x3.ToString();
+            }
+            if (m3 == 60.0)
+            {
+                m3 = 0.0;
+                ++h3;
+            }
+
         }
     }
 }
